6f8a3d3955259bf2c438626ff2cdbbf10cd8b635
Elfandi Rais Mahdavikia <elfandi.rm@gmail.com>
2026-03-14 00:30:14 +0700

    Percobaan 2: tampilan deskripsi

--- lib/main.dart
+++ lib/main.dart
@@ -35,6 +35,14 @@ class DetailScreen extends StatelessWidget {
                 style: TextStyle(fontSize: 30, fontWeight: FontWeight.w500),
               ),
             ),
+            Container(
+              padding: EdgeInsets.all(16),
+              child: Text(
+                "The Surabaya Submarine Monument,also known as Monumen Kapal Selam, is a unique monument located in Surabaya, Indonesia. It is a retired Russian-made submarine, the KRI Pasopati 410, that has been converted into a museum and a memorial for the Indonesian Navy. The submarine is displayed above ground and visitors can enter the vessel and explore its interior, which has been preserved to give a glimpse into life on a submarine. Visitors to the Surabaya Submarine Monument can explore the interior of the submarine and see the living quarters, control room, torpedo tubes, and other features that are typical of a submarine. The museum also has a range of exhibits and displays that showcase the history and achievements of the Indonesian Navy, as well as the importance of maritime defense for the country. One of the highlights of the museum is the interactive exhibits that allow visitors to experience what it is like to be on a submarine. Tourists can sit in a simulated control room and practice launching torpedoes or use virtual reality technology to explore the underwater world.",
+                textAlign: TextAlign.center,
+                style: TextStyle(fontSize: 16),
+              ),
+            ),
           ],
         ),
       ),
